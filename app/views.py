16f91346b77dd9fe84a1c4ec7c5a357eb52411a7
erik-nathan <eriknathan.contato@gmail.com>
2023-04-28 09:42:58 -0300

    feat: criando a função de create

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from app.forms import CarrosForm
 
 
@@ -11,3 +11,10 @@ def form(request):
     data = {}
     data['form'] = CarrosForm()
     return render(request, 'form.html', data)
+
+
+def create(request):
+    form = CarrosForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
