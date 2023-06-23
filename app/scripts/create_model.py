4595b2d8b22d7b63c490d7e21838c048c55c31cf
julioc-barros <juliocbarros339@gmail.com>
2023-06-23 18:26:52 -0300

    feat: criando IA 75%

--- app/scripts/create_model.py
+++ app/scripts/create_model.py
@@ -1,19 +1,42 @@
-import joblib
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
+import joblib
 
 # Carregar os dados do arquivo CSV
-data = pd.read_csv('/home/eriknathan/Estudos/Projetos/cadastro-automoveis-django/app/scripts/winequality-red.csv')
+data = pd.read_csv('C:\\Users\\Julio\\Documents\\Kropneus\\trabalho-ia-vinhos\\app\\scripts\\winequality-red.csv')
 
-# Separar os dados de entrada (X) e os rótulos (y)
-X = data.drop('quality', axis=1)
-y = data['quality']
+# Dividir os dados em recursos (features) e rótulos (labels)
+X = data.drop('quality', axis=1)  # Features
+y = data['quality']  # Rótulo de qualidade
 
-# Criar o classificador Random Forest
-rf = RandomForestClassifier()
-rf.fit(X, y)
+# Melhores hiperparâmetros encontrados
+best_params = {}
 
-# Salvar o modelo treinado em um arquivo h5
-joblib.dump(rf, 'random_forest.h5')      
-    
+# Dividir os dados em conjuntos de treinamento e teste
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
+
+# Definir os hiperparâmetros a serem ajustados
+params = {'n_estimators': 85,
+            'max_depth': 40,
+            'min_samples_split': 4
+            }
+
+# Criar o modelo RandomForestClassifier
+model = RandomForestClassifier(random_state=67, **params)
+
+# Treinar o modelo
+model.fit(X_train, y_train)
+
+# Fazer previsões no conjunto de teste
+y_pred = model.predict(X_test)
+
+# Calcular a acurácia
+current_accuracy = accuracy_score(y_test, y_pred)
+
+joblib.dump(model, 'random_forest.h5')
+
+print("Treinamento concluído!")
+print(f"Acurácia alcançada: {current_accuracy * 100}%")
